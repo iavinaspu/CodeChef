9f8e5d1742e8ebdbae7fd9e94eda1f8f9695d4e9
AVINASH PUJARI <pujariavinash97@gmail.com>
2021-06-22 17:47:40 +0530

    BUYPLSE

---
+++ BUYPLSE.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+#ifndef ONLINE_JUDGE
+freopen("input.txt", "r", stdin);
+freopen("output.txt", "w", stdout);
+#endif
+
+int a, b, x, y;
+cin >> a >> b >> x >> y;
+
+cout << a * x + b * y << "\n";
+}
