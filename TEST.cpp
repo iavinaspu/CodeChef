a508e849e289c171514890db683a793328dedcde
AVINASH PUJARI <pujariavinash97@gmail.com>
2021-06-24 11:48:15 +0530

    TEST

---
+++ TEST.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+#ifndef ONLINE_JUDGE
+freopen("input.txt", "r", stdin);
+freopen("output.txt", "w", stdout);
+#endif
+
+int N;
+while(N!=42){
+    cin >> N;
+    if(N==42){
+        break;
+    }
+    cout << N << "\n";
+}
+return 0;
+}
