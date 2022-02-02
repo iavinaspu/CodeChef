abed16c6d22e09b279110bcc35ac8d238e1b7221
AVINASH PUJARI <pujariavinash97@gmail.com>
2022-02-02 20:18:25 +0530

    BADMINTON

---
+++ BADMINTON.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+#ifndef ONLINE_JUDGE
+freopen("input.txt", "r", stdin);
+freopen("output.txt", "w", stdout);
+#endif
+
+int T{0};
+cin >> T;
+
+while(T--){
+    long long int n{0};
+    cin >> n;
+
+    cout << (n / 2) + 1 << endl;
+    }
+}
