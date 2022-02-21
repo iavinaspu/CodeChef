f9f5455d47ea178d645d9649c58cf7e400f699ad
AVINASH PUJARI <pujariavinash97@gmail.com>
2022-02-21 20:33:02 +0530

    BOTTOM

---
+++ guess_bottom.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+#ifndef ONLINE_JUDGE
+freopen("input.txt", "r", stdin);
+freopen("output.txt", "w", stdout);
+#endif
+
+int T{0};
+cin >> T;
+
+while(T--){
+    int X{0};
+    cin >> X;
+
+    cout << (7 - X) << endl;
+}
+}
