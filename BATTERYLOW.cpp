2487e574da088ad3f1a6041eca52fe800b21bffd
AVINASH PUJARI <pujariavinash97@gmail.com>
2022-03-01 20:20:07 +0530

    BATTERYLOW

---
+++ BATTERYLOW.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+#ifndef ONLINE_JUDGE
+freopen("input.txt", "r", stdin);
+freopen("output.txt", "w", stdout);
+#endif
+
+int T{0};
+cin >> T;
+
+while(T--){
+    int X{0};
+    cin >> X;
+
+    if(X<=15){
+        cout << "YES" << endl;
+    }
+    else{
+        cout << "NO" << endl;
+    }
+}
+}
